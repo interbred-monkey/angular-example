cccbdae62cae7146c134e95c184c5df771705b9c
Simon Mudd <smudd@amplience.com>
2020-07-02 15:48:48 +0100

    added window dimensions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  windowHeight: number;
+  windowWidth: number;
+
+  ngOnInit(): void {
+    this.windowHeight = window.innerHeight;
+    this.windowWidth = window.innerWidth;
+  }
+}
